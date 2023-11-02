2f233f5bddbc58a7c20dc39783d84324c8d95587
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-11-02 11:27:14 +0100

    Add dogs

---
+++ src/main/java/org/example/Dog.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Dog {
+
+    public static final boolean CARNIVORE = true;
+
+    private String name;
+    private int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static int calculateNextAge(int age) {
+        return age + 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Dog dog1 = new Dog("wuffi", 5);
+        Dog dog2 = new Dog("waldi", 7);
+
+        System.out.println(dog1);
+        System.out.println(dog2);
+
+        System.out.println(Dog.CARNIVORE);
+        System.out.println(Dog.calculateNextAge(9));
+
     }
 }
